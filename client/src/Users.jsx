b3de2d1fe6d9fc377a490dab6b29a181fb1e13f4
Ayomide Akintan <emzyakints2005@gmail.com>
2023-06-08 12:15:34 +0100

    Update

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -7,10 +7,19 @@ function Users() {
 
     useEffect(() => {
         axios.get('http://localhost:3001')
-        .then(result => setUsers(result.data))
-        .catch(err => console.log(err))
+            .then(result => setUsers(result.data))
+            .catch(err => console.log(err))
 
     }, [])
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/deleteUser/' + id)
+            .then(res => {
+                console.log(res)
+                window.location.reload()
+            })
+            .catch(err => console.log(err));
+    }
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
@@ -33,7 +42,8 @@ function Users() {
                                     <td>{user.age}</td>
                                     <td>
                                         <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
-                                        <button className='btn btn-danger'>Delete</button>
+                                        <button className='btn btn-danger'
+                                            onClick={(e) => handleDelete(user._id)}>Delete</button>
                                     </td>
                                 </tr>
                             })
